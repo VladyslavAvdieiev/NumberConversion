18b8711f6f73d06bd10d560ba0c15bf79ad9716e
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2018-12-19 14:19:34 +0200

    feat(NumberConverter): implement general methods

--- NumberConversion_CS/NumberConversion/NumberConverter.cs
+++ NumberConversion_CS/NumberConversion/NumberConverter.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace NumberConversion
 {
-    class NumberConverter
-    {
+    public static class NumberConverter {
+
+        /// <summary>
+        /// Converts integer part of number from any number system to decimal one.
+        /// </summary>
+        /// <param name="integerPartOfNumber">Any number in any number system.</param>
+        /// <param name="system">Number system of that number.</param>
+        public static string ConvertIntegerPartToDecimal(string integerPartOfNumber, int system) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Converts integer part of number from decimal number system to any one.
+        /// </summary>
+        /// <param name="integerPartOfNumber">Any number in decimal number system.</param>
+        /// <param name="system">Number system in which number should be be converted.</param>
+        public static string ConvertIntegerPartToAnySystem(string integerPartOfNumber, int system) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Converts fraction part of number from any number system to decimal one.
+        /// </summary>
+        /// <param name="fractionPartOfNumber">Any number in any number system.</param>
+        /// <param name="system">Number system of that number.</param>
+        public static string ConvertFractionPartToDecimal(string fractionPartOfNumber, int system) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Converts fraction part of number from decimal number system to any one.
+        /// </summary>
+        /// <param name="fractionPartOfNumber">Any number in decimal number system.</param>
+        /// <param name="system">Number system in which number should be converted.</param>
+        public static string ConvertFractionPartToAnySystem(string fractionPartOfNumber, int system) {
+            throw new NotImplementedException();
+        }
     }
 }
