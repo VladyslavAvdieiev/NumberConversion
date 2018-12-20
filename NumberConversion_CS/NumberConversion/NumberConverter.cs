e6a35df5ef554a6e07ded8045ff0d99c8c4db744
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2018-12-21 00:05:49 +0200

    refactor(NumberConverter.ConvertIntegerPartToDecimal): change implementation

--- NumberConversion_CS/NumberConversion/NumberConverter.cs
+++ NumberConversion_CS/NumberConversion/NumberConverter.cs
@@ -10,7 +10,7 @@ namespace NumberConversion
         /// <summary>
         /// Alphabet of number systems.
         /// </summary>
-        public static IList<char> Alphabet { get => _alphabet.AsReadOnly();  }
+        public static IList<char> Alphabet { get => _alphabet.AsReadOnly(); }
         private static List<char> _alphabet = new List<char> { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B',
                                                                'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
                                                                'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
@@ -21,11 +21,11 @@ namespace NumberConversion
         /// <param name="integerPartOfNumber">Any number in any number system.</param>
         /// <param name="system">Number system of that number.</param>
         public static string ConvertIntegerPartToDecimal(string integerPartOfNumber, int system) {
+            /*Converting mechanism*/
             int dec = 0;
             int degree = integerPartOfNumber.Length;
-
-            for (int i = 0; i < integerPartOfNumber.Length; i++)
-                dec += (int)(Alphabet.IndexOf(integerPartOfNumber[i]) * Math.Pow(system, --degree));
+            foreach (char digit in integerPartOfNumber)
+                dec += (int)(Alphabet.IndexOf(digit) * Math.Pow(system, --degree));
 
             return dec.ToString();
         }
