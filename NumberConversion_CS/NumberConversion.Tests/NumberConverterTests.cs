8e20847c13a150ad835c785f2b1c5d39b9d8c73b
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2018-12-21 00:16:22 +0200

    test: refactor valid test of ConvertIntegerPartToDecimal method

--- NumberConversion_CS/NumberConversion.Tests/NumberConverterTests.cs
+++ NumberConversion_CS/NumberConversion.Tests/NumberConverterTests.cs
@@ -6,15 +6,13 @@ namespace NumberConversion.Tests
     public class NumberConverterTests {
 
         [Theory]
-        [InlineData("3A", 33, "109")]
-        [InlineData("AD", 17, "183")]
-        [InlineData("423", 8, "275")]
+        [InlineData("3A", 33, "109")]   // uppercase of number
+        [InlineData("ad", 17, "183")]   // lowercase of number
+        [InlineData("423", 8, "275")]   // random number
+        [InlineData("25", 10, "25")]    // decimal to decimal
         public void ConvertIntegerPartToDecimal_ConvertIntegerNumberFromRandomNumberSystemToDecimal_Should_ConvertCorrectly(string number, int system, string expected) {
-            /*Arrange*/
-            string actual;
-
             /*Act*/
-            actual = NumberConverter.ConvertIntegerPartToDecimal(number, system);
+            string actual = NumberConverter.ConvertIntegerPartToDecimal(number, system);
 
             /*Assert*/
             Assert.Equal(expected, actual);
